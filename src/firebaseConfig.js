808dcdb33b8a5eeff3ba2c55f25128e224d6fd28
setsky95 <sebapapavlotsky@gmail.com>
2023-08-05 19:09:43 -0300

    variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import {getFirestore} from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpyGeVqD92pKk-mUYVamEYmdkkwBIasbk",
-  authDomain: "coder-react-dream-seller.firebaseapp.com",
-  projectId: "coder-react-dream-seller",
-  storageBucket: "coder-react-dream-seller.appspot.com",
-  messagingSenderId: "226160535141",
-  appId: "1:226160535141:web:4a678d8c7d51bcfa20888c"
+  apiKey:import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
